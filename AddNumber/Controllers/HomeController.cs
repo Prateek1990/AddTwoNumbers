c058b5d4c1cc054b39c236171be422e70974bb4c
Prateek <abhishekkr1410@gmail.com>
2018-10-04 01:45:36 +0530

    Add project files.

---
+++ AddNumber/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AddNumber.Models;
+
+namespace AddNumber.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            AddModel model = new AddModel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Add(AddModel model)
+        {
+            decimal Sum = model.FirstNumber + model.SecondNumber;
+            model.Sum = Sum;
+            return View("Index", model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AddNumber/Models/AddModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddNumber.Models
+{
+    public class AddModel
+    {
+
+        public decimal FirstNumber { get; set; }
+        public decimal SecondNumber { get; set; }
+        public decimal Sum { get; set; }
+    }
+}
